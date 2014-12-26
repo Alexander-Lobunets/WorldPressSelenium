c366218e16dc2da00cbcda072037617d3d7c5ff4
Alexander Lobunets <alexander.lobunets@hp.com>
2014-12-26 17:34:15 +0200

    1

--- src/main/java/ua/com/lohika/qa/bsm/publicside/MainPage.java
+++ src/main/java/ua/com/lohika/qa/bsm/publicside/MainPage.java
@@ -1,17 +1,20 @@
 package ua.com.lohika.qa.bsm.publicside;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+import ua.com.lohika.qa.bsm.Driver;
 import ua.com.lohika.qa.bsm.Page;
+
 
 /**
  * Created by alobunets on 12/26/2014.
  */
 public class MainPage extends Page {
     private WebDriver webDriver;
+    private static String url = "https://bsmqa.wordpress.com/";
 
     @FindBy(id = "title")
     private WebElement pageTitle;
@@ -19,8 +22,21 @@ public class MainPage extends Page {
 
     public MainPage(WebDriver webDriver){
         PageFactory.initElements(webDriver, this);
-        this.webDriver = webDriver;
+        this.webDriver = Driver.setWebDriver();
     }
+
+
+    public static void goTo() {
+        Driver.getDriver().navigate().to(url);
+    }
+
+    public static boolean at() {
+        return Driver.getInstance().findElement(By.id("login")).isDisplayed();
+    }
+
+
+
+
 
 
 
--- src/test/java/ua/com/lohika/qa/bsm/test/TestPublic.java
+++ src/test/java/ua/com/lohika/qa/bsm/test/TestPublic.java
@@ -2,11 +2,13 @@ package ua.com.lohika.qa.bsm.test;
 
 
 import org.testng.annotations.Test;
+import ua.com.lohika.qa.bsm.publicside.MainPage;
 
 public class TestPublic {
 
     @Test
-    public void TestSmthg(){
+    public void openMainPage(){
+        MainPage.
 
     }
 
