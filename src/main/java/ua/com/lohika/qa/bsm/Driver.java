fb924460c8fcc8bf854083317b7a9e2f26dbe2f2
Alexander Lobunets <alexander.lobunets@hp.com>
2014-12-26 16:14:03 +0200

    1

--- src/main/java/ua/com/lohika/qa/bsm/Driver.java
+++ src/main/java/ua/com/lohika/qa/bsm/Driver.java
@@ -1,10 +1,21 @@
 package ua.com.lohika.qa.bsm;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Driver {
     private static WebDriver webdriver;
 
+    private Driver(){
+        webdriver = new FirefoxDriver();
+        }
 
 
+    public WebDriver getDriver(){
+        return webdriver;
+    }
 }
+
+
+
+
